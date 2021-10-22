9693f21f73b8738a3f92bedced6f072657278ae9
Petia Valchkova <peps@gmail.com>
2021-10-22 22:17:32 +0300

    render

--- src/ProductDetalis/ProductDetalisApp.js
+++ src/ProductDetalis/ProductDetalisApp.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import classes from './ProductDetalisApp.module.css';
 
-import ProductData from '../ProductData';
 import Title from './Title';
-import ProductImg from './ProductImg';
+
 import Button from './Button';
 import ProductSecondPreview from './ProductSecondPreview';
 
 
 
 const ProductDetalisApp = (props) => {
+    const colorOptions = props.data.colorOptions.map((item, pos) => {
+        return (
+            <img className={classes.ProductImage}
+                src={item.imageUrl} alt={item.styleName}/>
+        );
+    })
     return (
         <div className={classes.ProductData}>
             <Title />
-            <ProductImg />
+
+            <h3 className={classes.SectionHeading}>Select Color</h3>
+           <div>
+           {colorOptions}
+           </div>
             <h3 className={classes.SectionHeading}>Features</h3>
             <Button />
             <button className={classes.PrimaryButton}>Buy Now</button>
+
+
             <ProductSecondPreview />
+
+
+
         </div>
+
     )
 }
 export default ProductDetalisApp
