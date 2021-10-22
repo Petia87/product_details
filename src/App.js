44626d3fbf9af95559f00db84e324dfffc599fde
Petia Valchkova <peps@gmail.com>
2021-10-22 22:18:00 +0300

    render

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-
+import React from 'react'
 import classes from './App.module.css';
 import Header from './Header';
 import ProductPreview from './ProductPreview'
 import ProductDetalisApp from './ProductDetalis/ProductDetalisApp'
+import ProductData from './ProductData';
+import { Component } from 'react';
 
 
-function App() {
+class App extends Component{
+  state = {
+    productData: ProductData
+  }
+  render() {
+    return (
+      <div className="classes.App">
+        <Header />
 
-  return (
-    <div className="classes.App">
-      <Header/>
+        <div className={classes.MainContainer}>
 
-      <div className={classes.MainContainer}>
- 
-        <ProductPreview />
-        <ProductDetalisApp />
-      
-
-
-      </div>
-    </div >
+          <ProductPreview />
+          <ProductDetalisApp data={this.state.productData}/>
 
 
-  );
+
+        </div>
+      </div >
+
+
+    );
+  }
 }
 
 export default App;
